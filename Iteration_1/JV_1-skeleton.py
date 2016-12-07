588c447a76a8ee5ee3cbed5022d59837d1979e5a
ljphillips84 <ljphillips84@users.noreply.github.com>
2016-12-07 11:49:40 +0000

    Update JV_1-skeleton.py

--- Iteration_1/JV_1-skeleton.py
+++ Iteration_1/JV_1-skeleton.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+# ---- Start Functions ---- #
 def getVoc(data):
     #
     #code goes here
@@ -30,10 +32,14 @@ def i_to_j(data):
     # code goes here
     #
     return data
+# ---- End Functions ---- #
+
+
+# ---- Main Program ----#
 
 # Import data into an array
 #
-results =
+data =
 #
 
 # Convert I to J
@@ -46,7 +52,7 @@ results =
 
 # When all the functions are completed, running the script should output all the parameters:
 
-print("Voc = " + str.format('{0:.2f}', getVoc(results)) + " V")
-print("Jsc = " + str.format('{0:.1f}', getJsc(results)) + " mA cm^-2")
-print("FF = " + str.format('{0:.1f}', 100*getFF(results)) + "%")
-print("Eff = " + str.format('{0:.1f}', getEff(results)) + "%")
+print("Voc = " + str.format('{0:.2f}', getVoc(data)) + " V")
+print("Jsc = " + str.format('{0:.1f}', getJsc(data)) + " mA cm^-2")
+print("FF = " + str.format('{0:.1f}', 100*getFF(data)) + "%")
+print("Eff = " + str.format('{0:.1f}', getEff(data)) + "%")
